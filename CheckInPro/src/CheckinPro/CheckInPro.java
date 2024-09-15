9edd4a30a0ac7335eac80a445e291802c2221fb5
valentino <166979527+Blutvogelspinne@users.noreply.github.com.>
2024-09-14 23:47:23 -0300

    Agrego el Programa

---
+++ CheckInPro/src/CheckinPro/CheckInPro.java
@@ -0,0 +1,15 @@
+
+package CheckinPro;
+
+import Vista.Inicio;
+
+
+public class CheckInPro {
+
+    public static void main(String[] args) {
+        Inicio N1= new Inicio();
+        
+        N1.setVisible(true);
+    }
+    
+}
