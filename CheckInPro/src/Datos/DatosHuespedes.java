14c9b4b8c920f0ee191a8fa675b9dbc06b61894a
AxelDev <axx.90@hotmail.com>
2024-09-16 23:40:26 -0300

    entrega uno

--- CheckInPro/src/Datos/DatosHuespedes.java
+++ CheckInPro/src/Datos/DatosHuespedes.java
@@ -1,13 +1,111 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Datos;
 
-/**
- *
- * @author Valen
- */
+import java.util.Objects;
+
+
 public class DatosHuespedes {
+    
+    private String nombre;
+    private String apellido;
+    private String correo ;
+    private int telefono;
+    private int documento;
+    
+ public DatosHuespedes(String nombre, String apellido, String correo, int telefono, int documento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.documento = documento;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    @Override
+    public String toString() {
+        return "DatosHuespedes{" + "nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo + ", telefono=" + telefono + ", documento=" + documento + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        hash = 29 * hash + Objects.hashCode(this.apellido);
+        hash = 29 * hash + Objects.hashCode(this.correo);
+        hash = 29 * hash + this.telefono;
+        hash = 29 * hash + this.documento;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DatosHuespedes other = (DatosHuespedes) obj;
+        if (this.telefono != other.telefono) {
+            return false;
+        }
+        if (this.documento != other.documento) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellido, other.apellido)) {
+            return false;
+        }
+        return Objects.equals(this.correo, other.correo);
+    }
+
+   
+    
+    
+    
     
 }
