1b038a497291c6d54fd79c3a775ac4babc839556
valentino <166979527+Blutvogelspinne@users.noreply.github.com.>
2024-10-03 11:28:16 -0300

    pre release

--- CheckInPro/src/CheckinPro/CheckInPro.java
+++ CheckInPro/src/CheckinPro/CheckInPro.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class CheckInPro {
 
@@ -21,12 +23,21 @@ public class CheckInPro {
         Connection cn;
         
         String url = "jdbc:mysql://localhost:3306/CheckInPro?user=root&password=1234";
+        String sqlInsert = "INSERT INTO Huespedes(nombre) VALUES(?); ";
+        
         
         try {
             cn = java.sql.DriverManager.getConnection(url);
             
             System.out.println("Conectado");
             
+            System.out.println("insertar Huesped");
+            System.out.println(h);
+            PreparedStatement pst = cn.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1, h.getNombre());
+            pst.execute();
+            
+            System.out.println(h);
             
         } catch (SQLException ex) {
             Logger.getLogger(CheckInPro.class.getName()).log(Level.SEVERE, null, ex);
